82416df0ef821e8e00c124dd696e2c78362909e0
sojip <sojipquantin@gmail.com>
2021-06-07 17:46:19 +0100

    adding marker to screen ok

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 const GameBoard = (function () {
-    let _game = [];
     let _gamePads = document.querySelectorAll(".gamePad");
-    let play = (marker) => {_game.push(marker)}
-    let populateDisplay =  () => {
-        for (let i = 0; i < _gamePads.length; i++) {
-            _gamePads[i].textContent = _game[i];
-        }
+    let _gameBoard = [];
+
+
+    updateGameboard = (_gamePad) => {
+        if (_gamePad.textContent === "") {
+            let player = Game.getCurrentPlayer()
+            player.addMark(_gamePad);
+            _gameBoard.push(player.marker)
+            console.log(_gameBoard)
+            Game.switchPlayer();           
+        }        
     }
+
+    _gamePads.forEach((_gamePad) => _gamePad.addEventListener('click', () => {updateGameboard(_gamePad)}))
+ 
+    function isOver() {
+        
+    }
+
     return {
-        populateDisplay,
-        play
+        _gameBoard
     }
 })()
 
 const Player = (name, marker) => {
+    let addMark = (_gamePad) => {
+         _gamePad.textContent = marker;
+    }
     return {
         name,
-        marker
+        marker,
+        addMark
     }
 }
 
+const Game = (function () {
+    let player1 = Player("tony", "X");
+    let player2 = Player("many", "O");
+    
+    function getCurrentPlayer() {
+        let player
+        document.querySelector(".currentlyPlaying").textContent === "1" ? player = player1 : player = player2;
+        return player;
+    }
+    
+    function switchPlayer() {
+        let playerId = document.querySelector(".currentlyPlaying").textContent;
+        playerId === "1" ? document.querySelector(".currentlyPlaying").textContent = "2" : document.querySelector(".currentlyPlaying").textContent = "1";
+        return
+    }
+
+    return {
+        getCurrentPlayer,
+        switchPlayer
+    }
+})();
 
