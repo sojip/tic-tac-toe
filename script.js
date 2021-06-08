8f154468c65f3b16dca6eb81183e5ad7afc56ed3
sojip <sojipquantin@gmail.com>
2021-06-08 20:15:50 +0100

    typo error fixed

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const GameBoard = (function () {
             if (Game.isOver()) {
                 _gamePads.forEach((gamePad) => gamePad.removeEventListener('click', _gamePadHandler));
             }
-            else Game.switchPlayer();        
-            return          
-        }        
+            else Game.switchPlayer();
+            return
+        }
     }
 
     _gamePadHandler = (e) => {
@@ -26,7 +26,7 @@ const GameBoard = (function () {
             gamePad.textContent = ""
             gamePad.addEventListener('click', _gamePadHandler)
         });
-        gameBoard.forEach((element, index, arr) => {arr[index] = ""});
+        gameBoard.forEach((element, index, arr) => { arr[index] = "" });
         document.querySelector(".currentlyPlaying").textContent = "1";
     }
 
@@ -39,8 +39,8 @@ const GameBoard = (function () {
 
 const Player = (name, marker) => {
     let addMark = (gamePad) => {
-         gamePad.textContent = marker;
-         return gamePad.dataset.num
+        gamePad.textContent = marker;
+        return gamePad.dataset.num
     }
     return {
         name,
@@ -53,19 +53,19 @@ const Game = (function () {
     let player1 = Player("tony", "X");
     let player2 = Player("many", "O");
 
-    let winningCombinations = [ 
-        [0, 1, 2], [3, 4, 5], 
+    let winningCombinations = [
+        [0, 1, 2], [3, 4, 5],
         [6, 7, 8], [0, 3, 6],
         [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ];
-    
+
     function getCurrentPlayer() {
         let player
         document.querySelector(".currentlyPlaying").textContent === "1" ? player = player1 : player = player2;
         return player;
     }
-    
+
     function switchPlayer() {
         let playerId = document.querySelector(".currentlyPlaying").textContent;
         playerId === "1" ? document.querySelector(".currentlyPlaying").textContent = "2" : document.querySelector(".currentlyPlaying").textContent = "1";
@@ -76,12 +76,12 @@ const Game = (function () {
         let player1Game = [];
         let player2Game = [];
         let end = false;
-        for(let i = 0; i < GameBoard.gameBoard.length; i++) {
-            if(GameBoard.gameBoard[i] === player1.marker) player1Game.push(i);
-            else if(GameBoard.gameBoard[i] === player2.marker) player2Game.push(i);
+        for (let i = 0; i < GameBoard.gameBoard.length; i++) {
+            if (GameBoard.gameBoard[i] === player1.marker) player1Game.push(i);
+            else if (GameBoard.gameBoard[i] === player2.marker) player2Game.push(i);
         }
 
-        
+
         //Look for 3 in a row
         winningCombinations.forEach((combination) => {
             if (combination.every((index) => {
@@ -94,7 +94,7 @@ const Game = (function () {
         })
 
         //Look for tie
-        if(player1Game.length + player2Game.length === 9 && end === false) end = null;
+        if (player1Game.length + player2Game.length === 9 && end === false) end = null;
 
         return end
     }
